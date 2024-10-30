e525c6f44af9adacda682e1d767903f93f4b7915
Nhat Toan <ntntoan1@gmail.com>
2024-10-30 10:29:37 +0700

    Fix Address Information space and Coupon TextField hint text.

--- lib/common/widgets/texts/textfields/coupon_textfield.dart
+++ lib/common/widgets/texts/textfields/coupon_textfield.dart
@@ -10,7 +10,6 @@ class CouponTextField extends StatelessWidget {
     super.key,
   });
 
-
   @override
   Widget build(BuildContext context) {
     final isDark = HelperFunctions.isDarkMode(context);
@@ -26,26 +25,31 @@ class CouponTextField extends StatelessWidget {
           // ----- TextField
           Flexible(
             child: TextFormField(
-              decoration: const InputDecoration(
-                  hintText: 'Have a Coupon code? Enter here',
-                  focusedBorder: InputBorder.none,
-                  disabledBorder: InputBorder.none,
-                  errorBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none),
+              decoration: InputDecoration(
+                hintText: 'Enter coupon code here',
+                focusedBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                hintStyle: Theme.of(context).textTheme.labelMedium,
+              ),
             ),
           ),
 
           // ----- Apply Button
           SizedBox(
             width: 80,
-            height: 50,
+            height: 60,
             child: ElevatedButton(
               onPressed: () {},
               style: ElevatedButton.styleFrom(
-                  foregroundColor: isDark ? AppPallete.whiteColor.withOpacity(0.7) : AppPallete.blackColor.withOpacity(0.7),
-                  backgroundColor: isDark ? AppPallete.darkGrey : AppPallete.greyColor,
-                  side: BorderSide(color: AppPallete.greyColor.withOpacity(0.2))
-              ),
+                  foregroundColor: isDark
+                      ? AppPallete.whiteColor.withOpacity(0.7)
+                      : AppPallete.blackColor.withOpacity(0.7),
+                  backgroundColor:
+                      isDark ? AppPallete.darkGrey : AppPallete.greyColor,
+                  side:
+                      BorderSide(color: AppPallete.greyColor.withOpacity(0.2))),
               child: const Text('Apply'),
             ),
           )
